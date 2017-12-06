6ecc0dc1fffb16a14175c61fff9a9f1e92ff0750
Chukwuma Ezumezu <chukwuma.ezumezu@andel.com>
2017-12-06 13:34:16 +0100

    add new route group: EmailClient

---
+++ routes/emailClient.js
@@ -0,0 +1,38 @@
+const emailClient = require('express').Router();
+const emailController = require('../controllers').emailController;
+
+/**
+ * @api {POST} /api/v1/emailClient/send Send Email
+ *
+ * @apiName Send Email
+ * @apiGroup Email Client
+ *
+ * @apiDescription Send email message to me.
+ *
+ * @apiParamExample {json} POST Request-Example:
+ {
+   "email": "Email of the sender",
+   "sbject": "Subject of the email",
+   "message": "The message you want to send".
+ }
+ *
+ * @apiSuccess {Object} response JSON Object
+ * @apiError {Object} error JSON Object with error message
+ *
+ *
+ * @apiSuccessExample {json} Success-Response:
+  {
+    "success": "Message sent successfully"
+  }
+ *
+ * @apiErrorExample {json} Error-Response:
+ {
+  "error": "There was an error sending mail. please try aagain later.
+  Or email me directly at chukwuma.ezumezu@gmail.com"
+ }
+**/
+emailClient.post('/send', (req, res, next) => {
+  emailController.sendEmail(req, res, next);
+});
+
+module.exports = emailClient;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const routes = require('express').Router();
 const syncBook = require('./syncbook');
+const emailClient = require('./emailClient');
 
 routes.get('/', (req, res) => {
   res.sendFile('../doc/index/html');
 });
 
 routes.use('/api/v1/sync-book', syncBook);
+
+routes.use('/api/v1/emailClient', emailClient);
 
 module.exports = routes;
